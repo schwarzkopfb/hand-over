00046aae3fd10371c4ebc9dff6aedbf52fe6e976
Balázs Schwarzkopf <schwarzkopfb@icloud.com>
2016-08-08 12:38:32 +0200

    improve test coverage

--- test/api.js
+++ test/api.js
@@ -12,51 +12,52 @@ var assert       = require('assert'),
 function noop() {
 }
 
-test.test('main', function (test) {
-    test.type(Handover, 'function', 'main export must be a function')
-    test.equal(Handover.Plugin, Plugin, 'Plugin constructor should be exposed')
-    test.equal(Handover.version, require('../package.json').version, 'package version should be exposed correctly')
+test.type(Handover, 'function', 'main export must be a function')
+test.equal(Handover.Plugin, Plugin, 'Plugin constructor should be exposed')
+test.equal(Handover.version, require('../package.json').version, 'package version should be exposed correctly')
 
-    var n = new Handover
-    test.type(n, EventEmitter, 'Handover should be derived from EventEmitter')
-    n = Handover()
-    test.type(n, Handover, 'constructor should work without `new`')
-    test.type(n.use, 'function', 'instance should have a `use()` fn')
-    test.type(n.send, 'function', 'instance should have a `send()` fn')
-    test.type(n.register, 'function', 'instance should have a `register()` fn')
-    test.type(n.unregister, 'function', 'instance should have a `unregister()` fn')
-    test.type(n.inspect, 'function', 'instance should have a custom `inspect()` fn')
-    test.type(n.inspect(), 'string', 'instance inspection should return a string')
+var n = new Handover
+test.type(n, EventEmitter, 'Handover should be derived from EventEmitter')
+n = Handover()
+test.type(n, Handover, 'constructor should work without `new`')
+test.type(n.use, 'function', 'instance should have a `use()` fn')
+test.type(n.send, 'function', 'instance should have a `send()` fn')
+test.type(n.register, 'function', 'instance should have a `register()` fn')
+test.type(n.unregister, 'function', 'instance should have a `unregister()` fn')
+test.type(n.inspect, 'function', 'instance should have a custom `inspect()` fn')
+test.type(n.inspect(), 'string', 'instance inspection should return a string')
 
-    test.throws(
-        function () {
-            n.save()
-        },
-        assert.AssertionError,
-        'accessing uninitialized required methods should throw'
-    )
-    test.throws(
-        function () {
-            n.load()
-        },
-        assert.AssertionError,
-        'accessing uninitialized required methods should throw'
-    )
-    test.throws(
-        function () {
-            n.remove()
-        },
-        assert.AssertionError,
-        'accessing uninitialized required methods should throw'
-    )
-    test.doesNotThrow(
-        function () {
-            n.transform(null, null, null, noop)
-        },
-        'accessing uninitialized not required  methods should not throw'
-    )
-    test.type(n.transform, 'function', '`transform` should have a default fn')
+test.throws(
+    function () {
+        n.save()
+    },
+    assert.AssertionError,
+    'accessing uninitialized required methods should throw'
+)
+test.throws(
+    function () {
+        n.load()
+    },
+    assert.AssertionError,
+    'accessing uninitialized required methods should throw'
+)
+test.throws(
+    function () {
+        n.remove()
+    },
+    assert.AssertionError,
+    'accessing uninitialized required methods should throw'
+)
+test.doesNotThrow(
+    function () {
+        n.transform(null, null, null, noop)
+    },
+    'accessing uninitialized not required  methods should not throw'
+)
+test.type(n.transform, 'function', '`transform` should have a default fn')
 
+test.test('chaining', function (test) {
+    // todo
     test.end()
 })
 
--- test/send.js
+++ test/send.js
@@ -21,30 +21,139 @@ TestPlugin.prototype.send = function (target, payload, callback) {
     callback()
 }
 
-var payload = {},
-    foo     = new TestPlugin('foo'),
-    bar     = new TestPlugin('bar'),
-    baz     = new TestPlugin('baz')
+var foo = new TestPlugin('foo'),
+    bar = new TestPlugin('bar'),
+    baz = new TestPlugin('baz')
 
 n.use(foo)
  .use(bar)
  .use(baz)
 
+function loadOne(userId, channel, callback) {
+    callback(null, 'moon')
+}
+
+function loadList(userId, channel, callback) {
+    callback(null, [ 'moon' ])
+}
+
+function loadEmptyList(userId, channel, callback) {
+    callback(null, [])
+}
+
+function loadMulti(userId, channel, callback) {
+    callback(null, [ 'mars', 'moon' ])
+}
+
+n.load = loadOne
 n.save = function (userId, channel, target, callback) {
     callback()
 }
 
-n.load = function (userId, channel, callback) {
-    callback(null, 'moon')
+var counter = 0,
+    pending = 0,
+    error
+
+function callback() {
+    var called
+    pending++
+
+    return function (err) {
+        if (called)
+            test.bailout('callback called more than once')
+        else
+            called = true
+
+        error = err
+        counter++
+        pending--
+    }
 }
 
-// todo: validate next tick
-n.send(1, payload, function (err) {
-    test.notOk(err, 'error should not be returned')
-    test.same(foo.lastTarget, 'moon', 'correct target should be used')
-    test.same(foo.lastPayload, payload, 'payload should be delivered')
-    test.same(bar.lastTarget, 'moon', 'correct target should be used')
-    test.same(bar.lastPayload, payload, 'payload should be delivered')
-    test.same(baz.lastTarget, 'moon', 'correct target should be used')
-    test.same(baz.lastPayload, payload, 'payload should be delivered')
-})
+function validate(channels, target, payload, count) {
+    channels.forEach(function (channel) {
+        test.same(channel.delivered, count, 'message should be delivered')
+
+        if (target)
+            test.same(channel.lastTarget, target, 'correct target should be used')
+
+        test.same(channel.lastPayload, payload, 'payload should be delivered')
+    })
+}
+
+var tests = [
+    // send to all channels
+    function () {
+        var payload = {}
+        n.send(1, payload, callback())
+        test.equal(counter, 0, 'callback should be called in the next tick')
+        return payload
+    },
+    // restricted channel list
+    function (payload) {
+        test.notOk(error, 'error should not be returned')
+        validate([ foo, bar, baz ], 'moon', payload, 1)
+        test.equal(counter, 1, 'callback should be called in the next tick')
+        n.send(1, 'foo bar', payload, callback())
+        return payload
+    },
+    // empty channel list
+    function (payload) {
+        test.notOk(error, 'error should not be returned')
+        validate([ foo, bar ], 'moon', payload, 2)
+        validate([ baz ], 'moon', payload, 1)
+        // make counters equal again for easier validation
+        baz.delivered++
+        n.send(1, [], payload, callback())
+        test.equal(counter, 2, 'callback should be called in the next tick')
+        return payload
+    },
+    function (payload) {
+        test.notOk(error, 'error should not be returned')
+        // note: delivery counters must remain the same
+        validate([ foo, bar, baz ], 'moon', payload, 2)
+        test.equal(counter, 3, 'callback should be called in the next tick')
+    },
+    // pass a single target as an array
+    function () {
+        n.load = loadList
+        n.send(1, 'spaceship', callback())
+    },
+    function () {
+        test.notOk(error, 'error should not be returned')
+        validate([ foo, bar, baz ], 'moon', 'spaceship', 3)
+        test.equal(counter, 4, 'callback should be called in the next tick')
+    },
+    // use multiple targets
+    function () {
+        n.load = loadMulti
+        n.send(1, 'animal', callback())
+    },
+    function () {
+        test.notOk(error, 'error should not be returned')
+        // note: two messages per channel,
+        // so counter has been increased with two
+        validate([ foo, bar, baz ], 'moon', 'animal', 5)
+        test.equal(counter, 5, 'callback should be called in the next tick')
+    },
+    // use a an empty target array
+    function () {
+        n.load = loadEmptyList
+        n.send(1, 'man', callback())
+    },
+    function () {
+        test.notOk(error, 'error should not be returned')
+        // note: delivery counters and payload must remain the same
+        validate([ foo, bar, baz ], 'moon', 'animal', 5)
+        test.equal(counter, 6, 'callback should be called in the next tick')
+    }
+    // todo: transformations
+    // todo: errors
+]
+
+void function next(i, val) {
+    var fn = tests[ i++ ]
+
+    if (fn)
+        process.nextTick(next, i, fn(val))
+}(0)
